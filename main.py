4ad058052e825b25d900d1558f421cb6c6b8e2a7
Kenisha Johnson <kenishaajohnson@gmail.com>
2022-07-11 19:59:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+# Dictionary, data type and data structure 
+# dictionary = {
+#   'a': 1, 
+#   'b': 2,
+#   'x': True
+# }
+
+dictionary = {
+  'a': [1,2,3],
+  'b': 'hello', 
+  'x': True
+}
+# variable does not have to be dictionary can be any variable name
+# print(dictionary['b']) #returns 2 
+
+#dictionary is unordered key value pair. unordered not next to each other in memory. 
+
+# print(dictionary)
+# print(dictionary['a'][1])
+
+my_list = [{
+  'a': [1,2,3], 
+  'b': 'hello',
+  'x': True
+}, 
+{
+  'a':[4,5,6],
+  'b': 'hello', 
+  'x': True 
+}]
+
+print(my_list[0]['a'][2])
+print(dictionary['a'][1])
